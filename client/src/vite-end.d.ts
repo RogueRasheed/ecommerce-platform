8500e4a442d061ba75e13a755532263c21e98d2e
Imoesi Rasheed Izuagbe <rasheedjay13@gmail.com>
2025-09-22 00:16:07 +0100

    yoo

---
+++ client/src/vite-end.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string; // 👈 your custom variable
+  // add more custom VITE_ vars if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+// Note: only variables prefixed with VITE_ are exposed to the client
